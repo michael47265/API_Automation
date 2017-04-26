a91ab2d80b2117e5ed389f550dcdbf58f7b0fd8b
michael47265 <michael47265@yahoo.com>
2017-04-26 16:31:44 -0400

    artOfTesting

---
+++ ArtOfTesting/src/test/java/artOfTesting/test/googleCalcStepDefinition.java
@@ -0,0 +1,67 @@
+package artOfTesting.test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class googleCalcStepDefinition {
+	
+	
+	protected WebDriver driver;
+	
+	 @Before
+	    public void setup() {
+		 System.setProperty("webdriver.chrome.driver", "C:/temp/chromedriver.exe");
+	        driver = new ChromeDriver();
+	}
+		
+	@Given("^I open google$")
+	public void I_open_google() {
+		//Set implicit wait of 10 seconds and launch google
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("http://www.google.com");
+	}
+	
+	@When("^I enter \"([^\"]*)\" in search textbox$")
+	public void I_enter_in_search_textbox(String additionTerms) {
+		//Write term in google textbox
+		WebElement googleTextBox = driver.findElement(By.id("lst-ib"));
+		googleTextBox.sendKeys(additionTerms);
+					
+		//Click on searchButton
+		WebElement searchButton = driver.findElement(By.id("_fZl"));
+		searchButton.click();
+	}
+	
+	@Then("^I should get result as \"([^\"]*)\"$")
+	public void I_should_get_correct_result(String expectedResult) {
+		//Get result from calculator
+		WebElement calculatorTextBox = driver.findElement(By.id("cwos"));
+		String result = calculatorTextBox.getText();
+				
+		//Verify that result of 2+2 is 4
+		System.out.println(result);
+		Assert.assertEquals(result, expectedResult);
+		
+		driver.close();
+	}
+	
+	 @After
+	    public void closeBrowser() {
+	        driver.quit();
+	 }
+
+}
+
+
